a333cd402511588c47091f915e5b58470ee9bf96
Ode Millington <odemillington05@gmail.com>
2023-11-06 11:49:40 -0400

    Changes login to php file

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="icon" type="image/x-icon" href="./Images/Favicon/favicon.ico">
     <title>WeWaffle | Login</title>
 </head>
