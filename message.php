9179566a3ce91766dd7803fa1f5df0321f2fe49c
Ode Millington <odemillington05@gmail.com>
2023-11-25 15:12:01 -0400

    Test

--- message.php
+++ message.php
@@ -1,31 +1,28 @@
+
 <?php
 
-$conn = mysqli_connect("localhost", "root", "", "wewafflesystem");
+    $conn = mysqli_connect("localhost", "root", "", "wewafflesystem");
 
-session_start();
+    session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$_SESSION['username'] = $username;
-$_SESSION['password'] = $password;
-
-
-$sql = "SELECT * FROM userlogin WHERE username = '$username' AND password = '$password'";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) == 1) {
-    $row = mysqli_fetch_assoc($result);
-    
-    echo "Welcome, " . $row['username'];
-    echo " Logged in successfully!";
-
-} else {
-    header("Location: index.php");
-    $_SESSION['error'] = ".";
-}
+    $_SESSION['username'] = $username;
+    $_SESSION['password'] = $password;
 
 
+    $sql = "SELECT * FROM userlogin WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($conn, $sql);
 
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        
+        echo "Welcome, " . $row['username'];
+        echo " Logged in successfully!";
 
+    } else {
+        header("Location: index.php");
+        $_SESSION['error'] = ".";
+    }
 ?>
